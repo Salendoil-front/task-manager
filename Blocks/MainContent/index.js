4374e4fb8caedfd9d53aa961efc0ef1b0427dbe7
Salendoil <ivantsovlexa@mail.ru>
2021-12-08 18:35:09 +0300

    refactor main layout

--- Blocks/MainContent/index.js
+++ Blocks/MainContent/index.js
@@ -4,6 +4,9 @@ const MainContentWrapper = styled.main`
   width: 85%;
   min-height: 100%;
   height: auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
 `
 
 export default function MainContent ({children}) {
